04ac22dbc093318c3fd282c51a8628c6fc9fa596
Irene Jaramillo <eyerene14@hotmail.com>
2020-11-07 13:32:48 -0800

    Workshop 4: Half way done with POST on second favorite router

---
+++ Desktop/5-NodeJS-Express-MongoDB/nucampsiteServer/notes.js
@@ -0,0 +1,5 @@
+const campsiteRouter = require("./routes/campsiteRouter");
+
+campsiteRouter._id1 = ObjectId("5fa6f6bb6ca9b043949670de");
+campsiteRouter._id2 = ObjectId("5fa6f6bb6ca9b043949670df");
+campsiteRouter._id3 = ObjectId("5fa6f6bb6ca9b043949670e0")
